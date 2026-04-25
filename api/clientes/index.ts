ae40e39637ce07fbce7592edfab36b19204d8896
Gusthavo Ramos Miguel <108017738+GusthavoRMiguel@users.noreply.github.com>
2026-04-25 13:19:02 -0300

    autenticacao duplicada

--- api/clientes/index.ts
+++ api/clientes/index.ts
@@ -7,19 +7,8 @@
 
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { listarClientes, criarCliente, editarCliente, deletarCliente } from '../../services/clientes.service.js';
-import { autenticarRequisicao, AuthError } from '../../api/_lib/auth.js';
-
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-  try {
-    autenticarRequisicao(req);
-  } catch (error: any) {
-    if (error instanceof AuthError) {
-      return res.status(error.statusCode).json({ erro: error.message });
-    }
-    return res.status(401).json({ erro: 'Requer autenticacao.' });
-  }
-
   switch (req.method) {
     case 'GET':
       return await listarClientes(req, res);
